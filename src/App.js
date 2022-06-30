2aa59ca305c87a98bcc2ebaf01baef9639ec1e9b
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-30 16:02:49 -0400

    Added home logo and component. Created logo_s, may go ahead and merge logo_s with logo to see if it looks better

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
   return (
     <>
     <Routes>
-    <Route path="/" element={<Layout />} />
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />}>
+      </Route>
+      </Route>
     </Routes>
     </>
   );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,18 @@
 import './index.scss'
+import LogoTitle from '../../assets/images/logo_s.png'
 
-const Home = () => ('Hello')
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm</h1>
+                <img src={LogoTitle} alt="developer" />
+                on
+                <br />
+                developer
+            </div>
+        </div>
+    )
+}
 
 export default Home
