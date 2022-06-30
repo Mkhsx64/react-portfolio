9d5c165fc63e50fbf922d759a623bd9e8388f6cc
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-29 23:02:25 -0400

    Finishing scss styling on Sidebar component and created Home component, linked to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './components/Home';
 
 function App() {
   return (
---
+++ src/components/Home/index.js
@@ -0,0 +1,5 @@
+import './index.scss'
+
+const Home = () => ('Hello')
+
+export default Home
