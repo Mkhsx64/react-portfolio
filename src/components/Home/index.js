7b17448e3710ad7addb29b5f772a795c034ef32b
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-30 20:59:24 -0400

    Added styles to container for all pages, finished link to contact page in home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,17 @@
 import './index.scss'
 import LogoTitle from '../../assets/images/logo_s.png'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm</h1>
+                <h1>Hi, <br /> I'm
                 <img src={LogoTitle} alt="developer" />
                 on
                 <br />
-                developer
+                developer</h1>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
     )
