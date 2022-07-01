ca30c05530124aff2e2fe5c3f5ae7cc2bec6ef3f
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-30 21:08:57 -0400

    Fixed logo and started adding styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,8 +6,8 @@ const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                <img src={LogoTitle} alt="developer" />
+                <h1>Hi, I'm
+                <br /><img src={LogoTitle} alt="developer" />
                 on
                 <br />
                 developer</h1>
