6a815877b75491e4a34e433d933726dc9580bb59
Mkhsx64 <jonbecher@microagenetworks.com>
2022-06-30 21:58:49 -0400

    Set up home component to have text from "Hi, I'm #on developer to be able to be animated individually

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,32 @@
 import './index.scss'
 import LogoTitle from '../../assets/images/logo_s.png'
 import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters';
+import { useState } from 'react';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['o', 'n']
+    const jobArray = ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br />I'm
-                <img src={LogoTitle} alt="developer" />
-                on
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
                 <br />
-                developer</h1>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray} 
+                idx={15}/>
+                <br />
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray} 
+                idx={17}/>
+                </h1>
                 <h2>Looking for a front-end/software developer role</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
