02c163285d22d7319223e5305a070e0e57300d27
Mkhsx64 <jonbecher@microagenetworks.com>
2022-07-05 15:50:54 -0400

    Fleshed out Contact component and added animated letter for contact me with an idx of 15

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,22 @@
 import './index.scss';
+import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
 
-const Contact = () => {('hi')};
+const Contact = () => {
+    return (
+        <>
+            <div className="container contact-page">
+                <div className='text-zone'>
+                    <h1>
+                    <AnimatedLetters strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                    idx={15}
+                    />
+                    </h1>
+                </div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
 
 export default Contact
